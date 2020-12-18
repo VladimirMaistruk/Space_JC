90caa8a26a4bfdc2b0440fe2cb32b4d1a2954cec
IT-BLOK\User <maistruk2001@gmail.com>
2020-12-18 21:43:45 +0200

    Игра про космос

---
+++ Assets/Scripts/EmitterScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EmitterScript : MonoBehaviour
+{
+
+    public GameObject asteroid;
+    public GameObject asteroid2;
+    public GameObject asteroid3;
+    public GameObject enemyShip;
+    public GameObject shield;
+    public float minDelay, maxDelay;//задержка между запусками
+
+    float nextLaunchTime;
+
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.time > nextLaunchTime)
+        {
+            
+            float xSize = transform.localScale.x / 2;
+            Vector3 newObjectPosition = new Vector3(
+                Random.Range(-xSize, xSize),
+                0,
+                transform.position.z
+                );
+            float rand = Random.Range(1,4);
+            GameObject newObject;
+            if (rand == 1)
+                newObject = enemyShip;
+            else if (rand == 2)
+                newObject = asteroid2;
+            else if (rand == 3)
+                newObject = asteroid3;
+            else 
+                newObject = asteroid;
+
+            Instantiate(newObject, newObjectPosition, Quaternion.identity);
+            nextLaunchTime = Time.time + Random.Range(minDelay, maxDelay);
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject[] hazards;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+
+    public UnityEngine.UI.Text scoreText;
+    public UnityEngine.UI.Text restartText;
+    public UnityEngine.UI.Text gameOverText;
+
+    private int score;
+    private bool gameOver;
+    private bool restart;
+    
+   
+
+     void Start()
+    {
+        gameOver = false;
+        restart = false;
+       
+        restartText.text = "";
+        gameOverText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine( SpawnWaves());
+    }
+
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (gameOver!=true)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                
+                GameObject hazard = hazards[Random.Range(0, hazards.Length)];
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x,
+                spawnValues.x), spawnValues.y, spawnValues.z);
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(hazard, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(spawnWait);
+            }
+
+            yield return new WaitForSeconds(waveWait);
+        }
+    }
+
+    
+
+    void Update()
+    {
+
+        
+
+
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            }
+
+
+        if (gameOver)
+        {
+            if (restart)
+            {
+                if (Input.GetKeyDown(KeyCode.R))
+                {
+                    UnityEngine.SceneManagement.SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+            }
+        }
+    }
+
+    public void GameOver()
+    {
+        restartText.text = "Press 'R' for Restart";
+        restart = true;
+        gameOverText.text = "Game Over!";
+        gameOver = true;
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score;
+    }
+
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+}
